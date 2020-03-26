cf36851020d41ba71f7d3b51c4e8c21a3255b41f
Pariatol <pierre.tromeur@gmail.com>
2020-03-26 21:42:03 +0100

    Updates

---
+++ precache-manifest.a9d1d7b81c1f3d77d795b216a6db4271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59807c0f369919d204831ea4d8cc470e",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "3f481108de266ed681e5",
+    "url": "/chatapp/static/css/main.5bb5b674.chunk.css"
+  },
+  {
+    "revision": "f2da28c71f020bbd6b2f",
+    "url": "/chatapp/static/js/2.bdefa077.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "/chatapp/static/js/2.bdefa077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f481108de266ed681e5",
+    "url": "/chatapp/static/js/main.93da30d5.chunk.js"
+  },
+  {
+    "revision": "6096f046118b4b57065b",
+    "url": "/chatapp/static/js/runtime-main.a9f38e48.js"
+  }
+]);
